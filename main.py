2e285f5d512897419a17611e0347c7142fba7945
Sookchand <34344439+Sookchand@users.noreply.github.com>
2023-10-24 13:21:32 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+tim.forward(300)
+# screen.setup(width=500, height=400)
+# # Use the textinput function to get user input
+# user_bet = screen.textinput(title="Make your bet", prompt="which turtle will win the race? Enter your color: ")
+#
+# print(user_bet)
+
+screen.exitonclick()
